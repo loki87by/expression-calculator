48a181c42dfa9cd97edaa7424a528aa90a986484
Алексей Акулич <61252310+loki87by@users.noreply.github.com>
2021-07-01 16:20:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
-function eval() {
-    // Do not use eval!!!
-    return;
-}
-
 function expressionCalculator(expr) {
-    // write your solution here
+    return eval(expr)
 }
 
 module.exports = {
