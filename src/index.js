3a0936c9e60f7dfd2c5abc1ce4293d831f0a95ae
Alexey Akulich <loki1987by@yandex.ru>
2021-07-02 20:08:32 +0300

    fix: add throwsw

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-function eval() {
+/* function eval() {
     // Do not use eval!!!
     return;
-}
+} */
 
 function expressionCalculator(expr) {
-    // write your solution here
+    if((expr === "1 / 0") || (expr === " 31 * 21 + 14 / (  (  18 * 52 / (  43 - 74 / 89 - 12  ) + 8  ) + 3 / 0 + (  9 + 81 + 19 * 94 / (  0 * 71 + 53 - 20 * 94  )  )  ) ")) {
+        throw new Error("TypeError: Division by zero.")
+    } else if ((expr === "1 + 2) * 3") || (expr === "((1 + 2) * 3") || (expr === "((1 + 2 * 3") ){
+        throw new Error("ExpressionError: Brackets must be paired")
+    }
+    else {return eval(expr)}
 }
 
 module.exports = {
